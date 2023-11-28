bf60f66a0f7f3f017d5015e48b8f4a91aaa59e70
PawelDuplaga <pawelduplagait@gmail.com>
2023-11-29 00:38:01 +0100

    +lenis scroll

--- app/index.js
+++ app/index.js
@@ -1,1 +1,23 @@
 import '../styles/index.scss';
+import Lenis from '@studio-freight/lenis';
+
+class App {
+    constructor(){
+        this._createLenis();
+        this._render();
+    }
+
+
+    _createLenis() {
+        this.Lenis = new Lenis({
+            lerp: 0.15,
+        });
+    }
+
+    _render() {
+        this.Lenis.raf();
+        requestAnimationFrame(this._render.bind(this));
+    }
+}
+
+new App();
